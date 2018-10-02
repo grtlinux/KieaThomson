3bc1f5303a0bf180c28c7b347f80ac2e9d2ede7f
grtlinux <grtlinux@gmail.com>
2018-10-02 16:57:59 +0900

    add check program

---
+++ KieaThomson/src/org/tain/test/HeaderMain.java
@@ -0,0 +1,15 @@
+package org.tain.test;
+
+public class HeaderMain {
+
+	public static void main(String[] args) {
+		byte[] header = {(byte)0x02, (byte)'R', (byte)'E', (byte)'U', (byte)'T' };
+
+		StringBuilder sb = new StringBuilder();
+		for (byte byt : header) {
+			sb.append(String.format("%02X ", byt));
+		}
+
+		System.out.println(">>>>> [" + sb.toString() + "]");
+	}
+}
