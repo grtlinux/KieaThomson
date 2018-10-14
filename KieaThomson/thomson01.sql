9e65ffeb9c4561bb4de6184186bc339d5cc8bc6f
grtlinux <grtlinuxr@gmail.com>
2018-10-15 00:41:17 +0900

    .

--- KieaThomson/thomson01.sql
+++ KieaThomson/thomson01.sql
@@ -9,6 +9,8 @@ CREATE TABLE `tain`.`thomson` (
 
 select count(*) from thomson;
 
+select message from thomson;
+
 select * from thomson where size > 64000;
 
 select * from thomson where size > 100000;
