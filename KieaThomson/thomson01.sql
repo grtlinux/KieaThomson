4286218a06de2ad6ae239cf7569d6dc4eac0d62f
grtlinux <grtlinuxr@gmail.com>
2018-10-15 00:30:14 +0900

    modify

--- KieaThomson/thomson01.sql
+++ KieaThomson/thomson01.sql
@@ -1,3 +1,12 @@
+CREATE TABLE `tain`.`thomson` ( 
+	`id`         INT                                                     NOT NULL AUTO_INCREMENT COMMENT 'id' 
+	, `guid`     VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL                COMMENT 'guid on message' 
+	, `size`     INT                                                     NOT NULL                COMMENT 'size of message' 
+	, `message`  LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci     NOT NULL                COMMENT 'message' 
+	, PRIMARY KEY (`id`)
+) ENGINE = InnoDB COMMENT = 'thomson table';
+
+
 select count(*) from thomson;
 
 select * from thomson where size > 64000;
