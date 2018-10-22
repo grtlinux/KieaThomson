758e218f3961c8ef3dcc23b378167c19eee48f93
grtlinux <grtlinux@gmail.com>
2018-10-22 18:14:38 +0900

    modify sources for new table

--- KieaThomson/src/org/tain/test/DbConnTestMain.java
+++ KieaThomson/src/org/tain/test/DbConnTestMain.java
@@ -27,58 +27,94 @@ public class DbConnTestMain {
 	public static void main(String[] args) throws Exception {
 		if (flag) log.info("INFO: {}", ClassUtils.getFileLine());
 		
-		if (!flag) test01(args);   // 13
+		if (flag) test01(args);   // 13 23 rec_count
+		if (flag) try { Thread.sleep(2000); } catch (InterruptedException e) {}
 		if (!flag) test02(args);   // 23
 		if (flag) test03(args);   // sync between 13 and 23
-		if (!flag) test04(args);   // select and insert
+		if (flag) try { Thread.sleep(2000); } catch (InterruptedException e) {}
+		if (flag) test04(args);   // select and insert into other tables
 	}
 	
 	private static void test01(String[] args) throws Exception {
 		if (!flag) log.info("INFO: {}", ClassUtils.getFileLine());
 		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet resultSet = null;
-		ResultSetMetaData metaData = null;
+		Connection conn13 = null;
+		Connection conn23 = null;
+		PreparedStatement pstmt13 = null;
+		PreparedStatement pstmt23 = null;
+		ResultSet resultSet13 = null;
+		ResultSet resultSet23 = null;
+		ResultSetMetaData metaData13 = null;
+		ResultSetMetaData metaData23 = null;
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://192.168.1.13:3306/tain", "root", "toor");
-			conn.setAutoCommit(false);
+			conn13 = DriverManager.getConnection("jdbc:mysql://192.168.1.13:3306/tain", "root", "toor");
+			conn23 = DriverManager.getConnection("jdbc:mysql://192.168.1.23:3306/tain", "root", "toor");
+			conn13.setAutoCommit(false);
+			conn23.setAutoCommit(false);
 
-			String sql = ""
+			String sql13 = ""
 					+ " select"
-					+ "    count(*) REC_COUNT"
+					+ "    count(*) REC_COUNT13"
 					+ " from"
 					+ "    THOMSON"
 					+ " where 1=1"
 					+ "";
 
-			pstmt = conn.prepareStatement(sql);
+			String sql23 = ""
+					+ " select"
+					+ "    count(*) REC_COUNT23"
+					+ " from"
+					+ "    THOMSON"
+					+ " where 1=1"
+					+ "";
 
-			resultSet = pstmt.executeQuery();
-			metaData = resultSet.getMetaData();
-			while (resultSet.next()) {
+			pstmt13 = conn13.prepareStatement(sql13);
+			pstmt23 = conn23.prepareStatement(sql23);
+
+			resultSet13 = pstmt13.executeQuery();
+			metaData13 = resultSet13.getMetaData();
+			while (resultSet13.next()) {
 				if (flag) System.out.printf(">>>>> 13: %d, %s = %s%n"
-						, metaData.getColumnCount()
-						, metaData.getColumnName(1)
-						, String.valueOf(resultSet.getObject(1))
+						, metaData13.getColumnCount()
+						, metaData13.getColumnName(1)
+						, String.valueOf(resultSet13.getObject(1))
+						);
+			}
+
+			resultSet23 = pstmt23.executeQuery();
+			metaData23 = resultSet23.getMetaData();
+			while (resultSet23.next()) {
+				if (flag) System.out.printf(">>>>> 13: %d, %s = %s%n"
+						, metaData23.getColumnCount()
+						, metaData23.getColumnName(1)
+						, String.valueOf(resultSet23.getObject(1))
 						);
 			}
 
 			if (flag)
-				conn.commit();
+				conn13.commit();
 			else
-				conn.rollback();
+				conn13.rollback();
+
+			if (flag)
+				conn23.commit();
+			else
+				conn23.rollback();
 		} catch (SQLException e) {
-			if (conn != null) try { conn.rollback(); } catch (Exception e2) {}
+			if (conn13 != null) try { conn13.rollback(); } catch (Exception e2) {}
+			if (conn23 != null) try { conn23.rollback(); } catch (Exception e2) {}
 			e.printStackTrace();
 		} catch (Exception e) {
-			if (conn != null) try { conn.rollback(); } catch (Exception e2) {}
+			if (conn13 != null) try { conn13.rollback(); } catch (Exception e2) {}
+			if (conn23 != null) try { conn23.rollback(); } catch (Exception e2) {}
 			e.printStackTrace();
 		} finally {
-			if (pstmt != null) try { pstmt.close(); } catch (Exception e) {}
-			if (conn != null) try { conn.close(); } catch (Exception e) {}
+			if (pstmt13 != null) try { pstmt13.close(); } catch (Exception e) {}
+			if (pstmt23 != null) try { pstmt23.close(); } catch (Exception e) {}
+			if (conn13 != null) try { conn13.close(); } catch (Exception e) {}
+			if (conn23 != null) try { conn23.close(); } catch (Exception e) {}
 		}
 	}
 
@@ -147,7 +183,7 @@ public class DbConnTestMain {
 			long maxId = 0;
 			if (flag) {
 				// get maxId from tgt
-				pstmtTgt = connTgt.prepareStatement("select max(ID) MAX_ID from THOMSON");
+				pstmtTgt = connTgt.prepareStatement("select ifnull(max(ID), 0) MAX_ID from THOMSON");
 				resultSetTgt = pstmtTgt.executeQuery();
 				while (resultSetTgt.next()) {
 					maxId = resultSetTgt.getLong("MAX_ID");
@@ -234,6 +270,7 @@ public class DbConnTestMain {
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		PreparedStatement pstmtInsert = null;
 		ResultSet resultSet = null;
 		ResultSetMetaData metaData = null;
 
@@ -242,7 +279,7 @@ public class DbConnTestMain {
 			conn = DriverManager.getConnection("jdbc:mysql://192.168.1.23:3306/tain", "root", "toor");
 			conn.setAutoCommit(false);
 
-			String sql = ""
+			String sqlSelect = ""   // not used
 					+ " select"
 					+ "    ID,"
 					+ "    GUID,"
@@ -251,25 +288,108 @@ public class DbConnTestMain {
 					+ " from"
 					+ "    THOMSON"
 					+ " where 1=1"
-					+ "    and ID > ?"
+					//+ "    and ID < ?"
+					+ "    and ID >= ?"
 					+ "";
+			
+			sqlSelect = ""
+					+ " select"
+					+ "    ID,"
+					+ "    GUID,"
+					+ "    SIZE,"
+					+ "    MESSAGE"
+					+ " from"
+					+ "    TAIN.THOMSON" 
+					+ " where 1=1"
+					+ "    and ID > ("
+					+ "        select ifnull(max(ID), 0) from TAIN.REUTERS"
+					+ "    )"
+					+ " order by"
+					+ "    ID "
+					+ " limit 100000"
+					+ "";
+			
+			String sqlInsert = ""
+					+ " insert into REUTERS"
+					+ " ("
+					+ "    ID,"
+					+ "    LANGUAGE,"
+					+ "    ACTIVDATE,"
+					+ "    TIMACTMS,"
+					+ "    ALTID,"
+					+ "    GUID,"
+					+ "    MIMETYPE,"
+					+ "    HEADLINE,"
+					+ "    BODY,"
+					+ "    ZIPSIZE,"
+					+ "    UDPSIZE,"
+					+ "    FIRSTCREATED,"
+					+ "    VERSIONcREATED"
+					+ " ) values ("
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?,"
+					+ "    ?"
+					+ " )";
 
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setLong(1, 150000);
+			pstmt = conn.prepareStatement(sqlSelect);
+			//pstmt.setLong(1, 1);
+			//pstmt.setLong(1, 100000);
 
 			resultSet = pstmt.executeQuery();
 			metaData = resultSet.getMetaData();
+			JSONObject jsonObject = null;
 			while (resultSet.next()) {
-				if (flag) System.out.printf(">>>>> [13] %s=%s, %s=%s%n"
+				jsonObject = new JSONObject(resultSet.getString(4));
+				
+				if (!flag) System.out.printf(">>>>> [13] %s=%s, %s=%s%n"
 						, metaData.getColumnName(1)
 						, resultSet.getString(1)
 						, metaData.getColumnName(4)
-						, new JSONObject(resultSet.getString(4)).toString(2)
+						, jsonObject.toString(2)
 						);
+				
+				if (flag) {
+					System.out.printf(">>>>> [ID:%7d] [GUID:%s]%n", resultSet.getInt("ID"), jsonObject.getString("GUID"));
+					
+					//System.out.println(">>>>> GUID        : " + jsonObject.getString("GUID"));
+					//System.out.println(">>>>> language    : " + jsonObject.getString("language"));
+					//System.out.println(">>>>> mimeType    : " + jsonObject.getString("mimeType"));
+					//System.out.println(">>>>> firstCreated: " + jsonObject.getString("firstCreated"));
+					//System.out.println(">>>>> headline    : " + jsonObject.getString("headline"));
+					//System.out.println(">>>>> body        : " + jsonObject.getString("body"));
+				}
+				
+				pstmtInsert = conn.prepareStatement(sqlInsert);
+				pstmtInsert.setInt   (1, resultSet.getInt("ID"));
+				pstmtInsert.setString(2,  jsonObject.getString("language")      );
+				pstmtInsert.setString(3,  jsonObject.getString("ACTIV_DATE")    );
+				pstmtInsert.setString(4,  jsonObject.getString("TIMACT_MS")     );
+				pstmtInsert.setString(5,  jsonObject.getString("altId")         );
+				pstmtInsert.setString(6,  jsonObject.getString("GUID")          );
+				pstmtInsert.setString(7,  jsonObject.getString("mimeType")      );
+				pstmtInsert.setString(8,  jsonObject.getString("headline")      );
+				pstmtInsert.setString(9,  jsonObject.getString("body")          );
+				pstmtInsert.setInt   (10, Integer.parseInt(jsonObject.getString("TOT_SIZE")));
+				pstmtInsert.setInt   (11, Integer.parseInt(resultSet.getString("SIZE")));
+				pstmtInsert.setString(12, jsonObject.getString("firstCreated")  );
+				pstmtInsert.setString(13, jsonObject.getString("versionCreated"));
+				pstmtInsert.executeUpdate();
 				
 				if (!flag) break;
 			}
 
+			if (!flag) System.out.println(">>>>> jsonObject = " + jsonObject.toString(2));
+			
 			if (flag)
 				conn.commit();
 			else
--- KieaThomson/thomson2301.sql
+++ KieaThomson/thomson2301.sql
@@ -1,3 +1,55 @@
+drop table tain.reuters;
+
+CREATE TABLE `tain`.`reuters` ( 
+	`id`               INT                                                      NOT NULL AUTO_INCREMENT COMMENT 'id on msg' 
+	, `language`       VARCHAR(8)                                               NOT NULL                COMMENT 'language on msg' 
+	, `activDate`      VARCHAR(16)                                              NOT NULL                COMMENT 'activeDate of msg' 
+	, `timactMs`       VARCHAR(16)                                              NOT NULL                COMMENT 'activeTime of msg' 
+	, `altId`          VARCHAR(32)   CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL                COMMENT 'altId of msg' 
+	, `guid`           VARCHAR(128)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL                COMMENT 'guid on msg' 
+	, `mimeType`       VARCHAR(128)                                             NOT NULL                COMMENT 'mimeType of msg' 
+	, `headline`       VARCHAR(4096) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL                COMMENT 'headline of msg' 
+	, `body`           LONGTEXT      CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL                COMMENT 'body of msg'
+	, `zipSize`        INT                                                      NOT NULL                COMMENT 'zipSize of msg' 
+	, `udpSize`        INT                                                      NOT NULL                COMMENT 'udpSize of msg' 
+	, `firstCreated`   VARCHAR(64)                                              NOT NULL                COMMENT 'firstCreated of msg' 
+	, `versionCreated` VARCHAR(64)                                              NOT NULL                COMMENT 'versionCreated of msg' 
+	, PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci COMMENT = 'reuters table';
+
+select count(*) from tain.reuters;
+
+
+---------------------------------------------------
+
+select max(id) from tain.reuters;
+
+select ifnull(max(id), 0) from tain.reuters;
+
+select
+	* 
+from
+	tain.thomson 
+where 1=1
+	and id > (
+		select ifnull(max(id), 0) from tain.reuters
+	)
+order by
+	id 
+limit 10
+;
+
+---------------------------------------------------
+select count(*) cnt_reuters from tain.reuters;
+select count(*) cnt_thomson from tain.thomson;
+---------------------------------------------------
+
+select * from tain.reuters order by udpsize desc limit 100;
+
+---------------------------------------------------
+---------------------------------------------------
+---------------------------------------------------
+
 select count(*) from thomson;
 
 select max(id) from thomson;
