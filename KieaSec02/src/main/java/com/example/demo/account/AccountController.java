283e031e980d452e7b3547ffa545e500c1e089d1
grtlinux <grtlinux@gmail.com>
2019-01-23 17:20:20 +0900

    .

--- KieaSec02/src/main/java/com/example/demo/account/AccountController.java
+++ KieaSec02/src/main/java/com/example/demo/account/AccountController.java
@@ -7,16 +7,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
 
+	private final boolean flag = true;
+	
+	@Autowired
+	private AccountRepository accountRepository;
+	
 	@Autowired
 	private AccountService accountService;
 	
 	@GetMapping("/create")
 	public Account create() {
-		Account account = new Account();
-		account.setEmail("grtlinux@gmail.com");
-		account.setPassword("{noop}kang123");
-		account.setPassword("kang123");
 		
-		return this.accountService.save(account);
+		if (flag) {
+			// repository
+			Account account = new Account();
+			account.setEmail("grtlinux@gmail.com");
+			account.setPassword("{noop}kang123");
+			
+			return this.accountRepository.save(account);
+		} else {
+			// service
+			Account account = new Account();
+			account.setEmail("grtlinux@gmail.com");
+			account.setPassword("kang123");
+			
+			return this.accountService.save(account);
+		}
 	}
 }
